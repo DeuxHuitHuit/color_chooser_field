fc2adba9623269bf5e24cb442310a7863941867d
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-05-29 14:37:16 -0400

    Code refactoring for Database methods

--- fields/field.colorchooser.php
+++ fields/field.colorchooser.php
@@ -46,9 +46,17 @@
 
 			$fields['field_id'] = $id;
 
-			Symphony::Database()->query("DELETE FROM `tbl_fields_".$this->handle()."` WHERE `field_id` = '$id' LIMIT 1");
-			return Symphony::Database()->insert($fields, 'tbl_fields_'.$this->handle());
-
+			Symphony::Database()
+				->delete('tbl_fields_' . $this->handle())
+				->where(['field_id' => $id])
+				->limit(1)
+				->execute()
+				->success();
+			return Symphony::Database()
+				->insert('tbl_fields_' . $this->handle())
+				->values($fields)
+				->execute()
+				->success();
 		}
 
 		function groupRecords($records){
@@ -68,7 +76,6 @@
 				}
 
 				$groups[$this->get('element_name')][$value]['records'][] = $r;
-
 			}
 
 			return $groups;
@@ -147,7 +154,7 @@
 			$brightness = round( (.2126 * $r + .7152 * $g + .0722 * $b) / 255 * 100 );
 			return $brightness;
 		}
-		
+
 		public function rgb2cmyk($r,$g,$b) {
 
 			$r = $r / 255;
@@ -231,17 +238,29 @@
 		}
 
 		public function createTable(){
-			return Symphony::Database()->query(
-				"CREATE TABLE IF NOT EXISTS `tbl_entries_data_".$this->get('id')."` (
-						  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-						  `entry_id` INT(11) UNSIGNED NOT NULL,
-						  `value` VARCHAR(32) DEFAULT NULL,
-						  PRIMARY KEY  (`id`),
-						  KEY `entry_id` (`entry_id`),
-						  KEY `value` (`value`)
-						) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
-
-			);
+			return Symphony::Database()
+				->create('tbl_entries_data_' . $this->get('id'))
+				->ifNotExists()
+				->charset('utf8')
+				->collate('utf8_unicode_ci')
+				->fields([
+					'id' => [
+						'type' => 'int(11)',
+						'auto' => true,
+					],
+					'entry_id' => 'int(11)',
+					'value' => [
+						'type' => 'varchar(32)',
+						'null' => true,
+					],
+				])
+				->keys([
+					'id' => 'primary',
+					'entry_id' => 'key',
+					'value' => 'key',
+				])
+				->execute()
+				->success();
 		}
 
 	}
